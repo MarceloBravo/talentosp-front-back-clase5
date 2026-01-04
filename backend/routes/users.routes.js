f3a4c4b9047d68e378667723aa1bd251427e066d
Marcelo Bravo <mabc@live.cl>
2026-01-04 17:40:00 -0300

    fix: Se cambió el nombre al campo del avatar

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -4,8 +4,9 @@ const UsersService = require('../services/users.service');
 const validaDatosUsuario = require('../middlewares/validaDatosUsuario.middleware');
 const authenticateToken = require('../middlewares/auth.middleware');
 const parseUserData = require('../middlewares/parseUserData.middleware');
-const upload = require('../middlewares/upload');
 const UploadFileService = require('../services/uploadFile.service');
+
+const upload = require('../middlewares/upload');
 
 module.exports = (app) => {
     const uploadFileService = new UploadFileService()
@@ -14,8 +15,8 @@ module.exports = (app) => {
 
     app.get('/api/users', (req, res, next) => userController.getAllUsers(req, res, next));
     app.get('/api/users/:id', (req, res, next) => userController.getUserById(req, res, next));
-    app.post('/api/users', authenticateToken, validaDatosUsuario, parseUserData, upload.single('attachments'), (req, res, next) => userController.createUser(req, res, next));
-    app.post('/api/register', validaDatosUsuario, parseUserData, upload.single('attachments'), (req, res, next) => userController.createUser(req, res, next));
-    app.put('/api/users/:id', authenticateToken, validaDatosUsuario, parseUserData, upload.single('attachments'), (req, res, next) => userController.updateUser(req, res, next));
+    app.post('/api/users', upload.single('avatar'), authenticateToken, validaDatosUsuario, parseUserData, (req, res, next) => userController.createUser(req, res, next));
+    app.post('/api/register', upload.single('avatar'), validaDatosUsuario, parseUserData, (req, res, next) => userController.createUser(req, res, next));
+    app.put('/api/users/:id', upload.single('avatar'), authenticateToken, validaDatosUsuario, parseUserData, (req, res, next) => userController.updateUser(req, res, next));
     app.delete('/api/users/:id', authenticateToken, (req, res, next) => userController.deleteUser(req, res, next));
 }
