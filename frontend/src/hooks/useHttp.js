8ff26e3da98f787fd8f125426f80c21c405618f1
Marcelo Bravo <mabc@live.cl>
2026-02-19 15:50:14 -0300

    Debug: Se agregó código de depuración en frontend\src\hooks\useHttp.js

--- frontend/src/hooks/useHttp.js
+++ frontend/src/hooks/useHttp.js
@@ -9,13 +9,13 @@ export const useHttp = () => {
     const sendRequest = async (url, method, data, config) => {
         try{
             setIsLoading(true)
-            debugger
             const result = await instance({
                 url,
                 method,
                 data, 
                 ...config
             })
+            console.log('result', result);
             if(result.data.code === 400){
                 setError(result.data.error)
                 return result.data
@@ -23,6 +23,7 @@ export const useHttp = () => {
             setData(result.data)
             return result.data
         }catch(err){
+            console.log('err', err);
             setError(err.response?.data?.error || (err.response?.data?.message || err.message))
         }finally{
             setIsLoading(false)
